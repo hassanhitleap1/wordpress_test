8a66bff10c9227742b919a9e163d30447ea26f48
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-09-14 14:28:14 -0700

    lesson 12

---
+++ wp-content/plugins/alccaddd-plugin/inc/Base/Activate.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package AlccadddPlgin 
+ */
+namespace Inc\Base;
+
+
+class Activate{
+
+
+    public static  function activate()
+    {
+        self::codex_custom_init();
+        flush_rewrite_rules();
+    }
+
+    public static function codex_custom_init()
+    {
+        
+        $args = array(
+          'public' => true,
+          'label'  => 'Books'
+          );
+        
+      register_post_type( 'book', $args );
+  }
+
+}
+?>
